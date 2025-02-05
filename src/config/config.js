cbde97760552cb3c04dcbd9194396c44e41d62af
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-02-05 17:43:58 +0900

    fix: config.js 모듈 바인딩 추가
    -DB2 관련 모듈이 빠져있었음

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import { CLIENT_VERSION, HOST, PORT } from '../constants/env.js';
 import { PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
-import { DB1_NAME, DB1_USER, DB1_PASSWORD, DB1_HOST, DB1_PORT } from '../constants/env.js';
+import { DB1_NAME, DB1_USER, DB1_PASSWORD, DB1_HOST, DB1_PORT, DB2_NAME, DB2_USER, DB2_PASSWORD, DB2_HOST, DB2_PORT } from '../constants/env.js';
 
 export const config = {
   server: {
