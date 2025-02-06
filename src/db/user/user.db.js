2bc17a7ccc078271ea16bf04b20d33b3e7211338
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-06 12:39:17 +0900

    feat : 쿼리문 수정
    트랜잭션 추가

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -7,9 +7,33 @@ export const findUserByDeviceID = async (deviceId) => {
   return toCamelCase(rows[0]);
 };
 
-export const createUser = async (deviceId) => {
-  await pools.USER_DB.query(SQL_QUERIES.CREATE_USER, [deviceId]);
-  return { deviceId };
+export const createUser = async (email, nickname, password) => {
+  const connection = await pools.USER_DB.getConnection();
+
+  try {
+    // 트랜잭션 시작
+    await connection.beginTransaction();
+
+    // 사용자 생성 쿼리 실행
+    const user = await connection.execute(SQL_QUERIES.CREATE_USER, [email, nickname, password]);
+
+    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 1]);
+    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 2]);
+    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 3]);
+    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 4]);
+    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 5]);
+
+    // 모든 쿼리가 성공하면 트랜잭션 커밋
+    await connection.commit();
+    return { success: true };
+  } catch (error) {
+    // 에러 발생 시 롤백
+    await connection.rollback();
+    throw error; // 에러를 다시 던져서 처리할 수 있도록 함
+  } finally {
+    // 연결을 반환하여 연결 풀에서 사용할 수 있도록 함
+    connection.release();
+  }
 };
 
 export const updateUserLogin = async (id) => {
--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,7 +1,7 @@
 export const SQL_QUERIES = {
-  FIND_USER_BY_DEVICE_ID: 'SELECT * FROM user WHERE device_id = ?',
+  FIND_USER: 'SELECT * FROM user WHERE device_id = ?',
   CREATE_USER: 'INSERT INTO user (email,nickname,password) VALUES (?,?,?)',
-  CREATE_CHARACTER : 'INSERT INT Character (userId,charStatId,gold,level,exp) VALUSE (?,?,?,?,?)'
+  CREATE_CHARACTER: 'INSERT INT Characters (userId,charStatId) VALUSE (?,?)',
   UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE device_id = ?',
   UPDATE_USER_LOCATION: 'UPDATE user SET x_coord = ?, y_coord = ? WHERE device_id = ?',
 };
