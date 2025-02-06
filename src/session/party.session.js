3064892d08ec3aa4e42d2540a6d249393fb704ae
ssy1248 <ssy1248@gmail.com>
2025-02-06 11:58:14 +0900

    feat partyClass, constants.js

--- src/session/party.session.js
+++ src/session/party.session.js
@@ -1,6 +1,8 @@
 import Party from "../classes/models/party.class";
 import { partySessions } from "./sessions";
 
-export const createPartySession = (partyName) => {
-
+export const createPartySession = (id, partyName) => {
+    const partySession = new Party(id, partyName);
+    partySessions.push(partySession);
+    return partySession;
 }
