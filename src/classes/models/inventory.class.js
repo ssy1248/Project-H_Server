9468f97dce9591d00b21154d697e0e200d40e979
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-02-06 18:08:09 +0900

    feat: 인벤토리, 아이템 클래스 추가

---
+++ src/classes/models/inventory.class.js
@@ -0,0 +1,26 @@
+class Inventory{
+    constructor(){
+        this.ineventory = []; // 소지중인 아이템 인벤토리
+        this.equipment = []; // 장비중인 아이템 인벤토리
+    }
+
+    // 아이템 장비하기
+    equip(item){
+        this.equipment.add(item);
+    }
+
+    // 아이템 해제하기
+    unequip(item){
+        this.equipment.remove(item);
+    }
+
+    // 인벤토리에 아이템 추가
+    add(item){
+        this.inventory.add(item);
+    }
+
+    // 인벤토리에서 아이템 제거
+    drop(item){
+        this.inventory.remove(item);
+    }
+}
