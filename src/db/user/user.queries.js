15a244add6c95df299944b930bbf30ca4f19f724
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-06 15:30:28 +0900

    feat : 유저 쿼리문 추가
    추가입니다.

--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,6 +1,7 @@
 export const SQL_QUERIES = {
-  FIND_USER: 'SELECT * FROM user WHERE device_id = ?',
-  CREATE_USER: 'INSERT INTO user (email,nickname,password) VALUES (?,?,?)',
+  FIND_USER: 'SELECT * FROM User WHERE email = ?',
+  FIND_CHARACTER: 'SELECT * FROM Characters WHERE userid = ?',
+  CREATE_USER: 'INSERT INTO User (email,nickname,password) VALUES (?,?,?)',
   CREATE_CHARACTER: 'INSERT INT Characters (userId,charStatId) VALUSE (?,?)',
   UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE device_id = ?',
   UPDATE_USER_LOCATION: 'UPDATE user SET x_coord = ?, y_coord = ? WHERE device_id = ?',
