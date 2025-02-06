6de1dc79cafc36053a4e00dddc6a11cb8d076388
ssy1248 <ssy1248@gmail.com>
2025-02-06 10:14:20 +0900

    feat partyClass, partySession

---
+++ src/classes/models/party.class.js
@@ -0,0 +1,10 @@
+class Party {
+    constructor(partyName) {
+        // 파티 이름
+        this.partyName = partyName; 
+        // 파티 인원을 담은 배열
+        this.partyMemebers = [];
+    }
+}
+
+export default Party;
---
+++ src/session/party.session.js
@@ -0,0 +1,6 @@
+import Party from "../classes/models/party.class";
+import { partySessions } from "./sessions";
+
+export const createPartySession = (partyName) => {
+
+}
