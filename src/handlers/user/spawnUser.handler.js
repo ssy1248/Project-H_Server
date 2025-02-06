5466e8b19e39b0d2596839520468459331a53334
Hwang20241024 <ghkdaksdbs@naver.com>
2025-02-06 09:31:10 +0900

    fix: createResponse.js 빅엔디안을 리틀엔디안으로 변경

---
+++ src/handlers/user/spawnUser.handler.js
@@ -0,0 +1,19 @@
+import { addUser } from '../../session/user.session.js'
+import { User } from '../../classes/models/user.class.js'
+
+const spawnUserHandler = async (socket, packetData) => {
+  console.log('테스트');
+
+  // [C_SelectCharacterRequest 패킷을 받는다] 
+  // nickname, class
+
+  // [세션에 유저를 생성한다.]
+
+  // [내정보를 보낸다] 
+  // 플레이어정보 , 상점 리스트.
+
+  // [내정보를 브로드 캐스트.]
+  // 플레이어 정보. 
+};
+
+export default spawnMonsterHandler;
