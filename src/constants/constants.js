27499eda632528ccc04ee21b1bbf3baa8e7a2103
Ju YongChan <tjszhf9616@naver.com>
2025-02-07 16:58:13 +0900

    style : 불필요한 주석 제거
    코드컨벤션 규칙에 따라 잘못작성한 주석 제거

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -11,9 +11,9 @@ export const ChatType = {
 };
 export const ChatErrorCodes = {
   // 0: 성공, 1: 권한 없음, 2: 파티 미가입, 3: 대상 없음, 4: 잘못된 채팅 타입
-  NONE: 0, // 성공
-  NO_PERMISSION: 1, // 권한 없음
-  NO_PARTY: 2, // 파티 미가입
-  USER_NOT_FOUND: 3, // 귓속말 대상 없음
-  INVALID_TYPE: 4, // 잘못된 채팅 타입
+  NONE: 0,
+  NO_PERMISSION: 1,
+  NO_PARTY: 2,
+  USER_NOT_FOUND: 3,
+  INVALID_TYPE: 4,
 };
