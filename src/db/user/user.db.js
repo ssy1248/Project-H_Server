e946ee1c779de87f5e68a1767befb359d6c99929
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-07 11:31:46 +0900

    fix : 유저 생성 트랜잭션 삭제
    유저만 생성하는걸로 변경

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -2,37 +2,19 @@ import pools from '../database.js';
 import { SQL_QUERIES } from './user.queries.js';
 import { toCamelCase } from '../../utils/transformCase.js';
 
+// 유저 찾기
 export const findUserEmail = async (email) => {
   return await pools.USER_DB.query(SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [email]);
 };
-
+// 유저 생성
 export const createUser = async (email, nickname, password) => {
   const connection = await pools.USER_DB.getConnection();
-
-  try {
-    // 트랜잭션 시작
-    await connection.beginTransaction();
-
-    // 사용자 생성 쿼리 실행
-    const user = await connection.execute(SQL_QUERIES.CREATE_USER, [email, nickname, password]);
-
-    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 1]);
-    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 2]);
-    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 3]);
-    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 4]);
-    await connection.execute(SQL_QUERIES.CREATE_CHARACTER, [user.id, 5]);
-
-    // 모든 쿼리가 성공하면 트랜잭션 커밋
-    await connection.commit();
+  // 사용자 생성 쿼리 실행
+  const user = await connection.execute(SQL_QUERIES.CREATE_USER, [email, nickname, password]);
+  if (user) {
     return { success: true };
-  } catch (error) {
-    // 에러 발생 시 롤백
-    await connection.rollback();
-    return { success: false };
-  } finally {
-    // 연결을 반환하여 연결 풀에서 사용할 수 있도록 함
-    connection.release();
   }
+  return { success: false };
 };
 
 export const updateUserLogin = async (id) => {
