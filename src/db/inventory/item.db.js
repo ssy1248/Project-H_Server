76f71b9f2686f61c4d23bcbbcf64a24cf2317de4
Hwang20241024 <ghkdaksdbs@naver.com>
2025-02-11 17:53:58 +0900

    feat: 아이템,스킬 추가/삭제/수정 구현

--- src/db/inventory/item.db.js
+++ src/db/inventory/item.db.js
@@ -22,6 +22,17 @@ export const createItem = async (name, itemType, stat, price) => {
     }
 };
 
+export const createItem2 = async (name, itemType, stat, price) => {
+    try {
+        const [result] = await pools.USER_DB.query(SQL_QUERIES.CREATE_ITEM, [name, itemType, stat, price]);
+        const newId = result.insertId;
+        return { success: true, id: newId };
+    } catch (error) {
+        console.log(error);
+        return { success: false, id: null };
+    }
+};
+
 export const updateItem = async (itemId, name, itemType, stat, price) => {
     try {
         const [result] = await pools.USER_DB.query(SQL_QUERIES.UPDATE_ITEM, [name, itemType, stat, price, itemId]);
@@ -34,9 +45,17 @@ export const updateItem = async (itemId, name, itemType, stat, price) => {
 
 export const deleteItem = async (itemId) => {
     try {
-        await pools.USER_DB.query(SQL_QUERIES.DELETE_ITEM, [itemId]);
+        const [result] = await pools.USER_DB.query(SQL_QUERIES.DELETE_ITEM, [itemId]);
+        return result.affectedRows > 0;
     } catch (error) {
         console.log(error);
     }
 };
+
+// 아이템 컬럼명만 가져오기.
+export const getItemsTableStructure = async () => {
+    const query = 'DESCRIBE Items';
+    const [results] = await pools.USER_DB.query(query);
+    return results.map((column) => column.Field); // 컬럼명만 반환
+  };
 
