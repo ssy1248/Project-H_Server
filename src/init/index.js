15f28d69f30cff498aef6cab46cccb706d74eac2
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-17 10:55:23 +0900

    feat : 아이템 새션 초기화
    입니다.

--- src/init/index.js
+++ src/init/index.js
@@ -2,10 +2,12 @@ import { loadProtos } from './loadProtos.js';
 import { v4 as uuidv4 } from 'uuid';
 import { addGameSession } from '../session/game.session.js';
 import { initMarketSesion } from '../session/market.session.js';
+import { initItemSesion } from '../session/item.session.js';
 
 const initServer = async () => {
   try {
     await loadProtos();
+    await initItemSesion();
     await initMarketSesion();
     const gameId = uuidv4();
     const gameSession = addGameSession(gameId);
