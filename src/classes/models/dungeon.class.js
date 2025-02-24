8be9ae90cef9997bab15b820f5b24af8b4e797ad
hyh-test <whdgl3615@naver.com>
2025-02-24 20:18:36 +0900

    feat : 던전 세션 스텟추가
    -던전 세션안에 스텟 추가

--- src/classes/models/dungeon.class.js
+++ src/classes/models/dungeon.class.js
@@ -1,4 +1,5 @@
 import { searchPartySession } from '../../session/party.session.js';
+import { getUserByNickname } from '../../session/user.session.js';
 import IntervalManager from '../managers/interval.manager.js';
 
 class Dungeon {
@@ -13,9 +14,9 @@ class Dungeon {
     this.isState = 'matching';
     // 몬스터 종류
     this.monsterId = [];
-    // 앞으로 추가할것들은 나올수 있는 몬스터 종류
+    // 플레이스텟 인포를 넣는 배열로
+    this.playerStatus = {};
   }
-
 
   //파티 인포 넣기
   setPartyInfo(party) {
@@ -44,6 +45,124 @@ class Dungeon {
     this.isState = state;
   }
 
+  getPlayerStatus(nickname) {
+    // nickname을 키로 사용하여 playerStatInfo를 가져옵니다.
+    const playerStat = this.playerStatus[nickname];
+
+    // 만약 playerStat이 존재하지 않으면 (존재하지 않는 닉네임인 경우)
+    if (!playerStat) {
+      // 없을 경우 기본값이나 오류 처리 반환
+      console.log(`${nickname} 닉네임을 가진 플레이어를 찾을 수 없습니다.`);
+      return null; // 혹은 {} 빈 객체를 반환할 수도 있습니다.
+    } else {
+      return playerStat;
+    }
+  }
+
+  setPlayerStatus(nickname) {
+    const user = getUserByNickname(nickname);
+
+    //이게 들어오면 객체 하나 생성
+    if (user) {
+      const playerStat = user.playerStatInfo;
+      //추가 할수 있는 방법으로
+      this.playerStatus[nickname] = {
+        hp: playerStat.hp,
+        maxHp: playerStat.maxHp,
+        mp: playerStat.mp,
+        maxMp: playerStat.maxMp,
+        atk: playerStat.atk,
+        def: playerStat.def,
+        speed: playerStat.speed,
+      };
+    }
+  }
+
+  // 플레이어의 HP 값 가져오기
+  getPlayerHp(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.hp : null;
+  }
+
+  // 플레이어의 HP 값 설정하기
+  setPlayerHp(nickname, newHp) {
+    this.updatePlayerStat(nickname, 'hp', newHp);
+  }
+
+  // 플레이어의 최대 HP 값 가져오기
+  getPlayerMaxHp(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.maxHp : null;
+  }
+
+  // 플레이어의 최대 HP 값 설정하기
+  setPlayerMaxHp(nickname) {
+    this.updatePlayerStat(nickname, 'maxHp', newHp);
+  }
+
+  // 플레이어의 MP 값 가져오기
+  getPlayerMp(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.mp : null;
+  }
+
+  // 플레이어의 MP 값 설정하기
+  setPlayerMp(nickname, newMp) {
+    this.updatePlayerStat(nickname, 'mp', newMp);
+  }
+
+  // 플레이어의 최대 MP 값 가져오기
+  getPlayerMaxMp(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.maxMp : null;
+  }
+
+  // 플레이어의 최대 MP 값 설정하기
+  setPlayerMaxMp(nickname) {
+    this.updatePlayerStat(nickname, 'maxMp', newHp);
+  }
+
+  // 플레이어의 공격력 가져오기
+  getPlayerAtk(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.atk : null;
+  }
+
+  // 플레이어의 방어력 가져오기
+  getPlayerDef(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.def : null;
+  }
+
+  // 플레이어의 스피드 가져오기
+  getPlayerSpeed(nickname) {
+    const playerStat = this.getPlayerStatus(nickname);
+    return playerStat ? playerStat.speed : null;
+  }
+
+  // 플레이어의 공격력 설정하기
+  setPlayerAtk(nickname, newAtk) {
+    this.updatePlayerStat(nickname, 'atk', newAtk);
+  }
+
+  // 플레이어의 방어력 설정하기
+  setPlayerDef(nickname, newDef) {
+    this.updatePlayerStat(nickname, 'def', newDef);
+  }
+
+  // 플레이어의 스피드 설정하기
+  setPlayerSpeed(nickname, newSpeed) {
+    this.updatePlayerStat(nickname, 'speed', newSpeed);
+  }
+
+  // 내부적으로 플레이어 상태를 업데이트하는 메서드
+  updatePlayerStat(nickname, statKey, newValue) {
+    const playerStat = this.getPlayerStatus(nickname);
+    if (playerStat) {
+      playerStat[statKey] = newValue;
+    }
+  }
+
   //나오는 몬스터 아이디 설정
   setMonsterId(id) {
     this.monsterId.push(id);
@@ -54,5 +173,4 @@ class Dungeon {
     return this.monsterId;
   }
 }
-
 export default Dungeon;
