bcc7bed8d22bd15c8e2bb07114c0dbc0cf11a874
bakfox <153157605+bakfox@users.noreply.github.com>
2025-02-25 16:07:29 +0900

    feat :  스폰 데이터 추가
    입니다.

--- src/handlers/game/dungeon/dungeonSpawn.handler.js
+++ src/handlers/game/dungeon/dungeonSpawn.handler.js
@@ -5,20 +5,46 @@ import { getUserByNickname, getUserBySocket } from '../../../session/user.sessio
 import { createResponse } from '../../../utils/response/createResponse.js';
 
 const dungeonSpawnHandler = async (socket, payload) => {
-  const user = getUserBySocket(socket);
-  const dungeondata = getDungeonSession(user.inDungeonId); // 파티안에 플레이어들 찾아오기!
+  try {
+    const user = getUserBySocket(socket);
+    if (!user) {
+      throw new Error('유저가 없습니다!');
+    }
+    const dungeondata = getDungeonSession(user.inDungeonId); // 파티안에 플레이어들 찾아오기!
+    if (!dungeondata) {
+      throw new Error('해당 던전이 없습니다!');
+    }
+    // 나중에 싱크 추가되면 변경
+    // for (let player of partyPlayers) {
+    //   userData.push(getUserByNickname(player.playerName));
+    // }
 
-  // 나중에 싱크 추가되면 변경
-  // for (let player of partyPlayers) {
-  //   userData.push(getUserByNickname(player.playerName));
-  // }
+    const dungeonInfo = {
+      dungeonId: '',
+      partyInfo: '',
+      dungeonState: '',
+      monsterId: [],
+    };
+    const transformInfo = [];
 
-  const dungeonInfo = {
-    dungeonId: '',
-    partyInfo: '',
-    dungeonState: '',
-    monsterId: [],
-  };
-  const packet = createResponse('dungeon', 'S_DungeonSpawn', PACKET_TYPE);
+    for (let data of dungeondata.playersTransform) {
+      const transform = {
+        posX: data.x,
+        posY: data.y,
+        posZ: data.z,
+        rot: data.rot,
+      };
+      transformInfo.push(transform);
+    }
+
+    const packet = createResponse('dungeon', 'S_DungeonSpawn', PACKET_TYPE.S_DUNGEONSPAWN, {
+      userId: user.userInfo.userId,
+      dungeonInfo,
+      transformInfo,
+    });
+    socket.write(packet);
+  } catch (err) {
+    console.log(err);
+  }
 };
 export default dungeonSpawnHandler;
