16de22f66b5bdcd664ec8953b9321d3bb6794fea
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2025-02-26 15:21:14 +0900

    feat: addAllStat
    - 스탯 정보를 입력하면 모든 스탯을 더한 새로운 객체를 반환하는 함수

--- src/classes/models/inventory.class.js
+++ src/classes/models/inventory.class.js
@@ -161,11 +161,20 @@ export default class Inventory {
     }
 
     // 모든 장착한 아이템의 스탯 합을 구하는 함수
-    getAllStat(){
+    getAllStat() {
         return this.getEquipment.reduce(
-            (acc, item) => acc + item.stat, 
+            (acc, item) => acc + item.stat,
             0
         );
     }
-    // 
+
+    // 클래스 기본 스탯에 장비한 모든 스탯을 더한 객체를 반환하는 함수
+    addAllStat(playerStatInfo) {
+        const copy = { ...playerStatInfo };
+        const stat = this.getAllStat();
+        for (const [key, value] of Object.entries(copy)) {
+            value += stat;
+        }
+        return copy;
+    }
 }
