aac85b815316c349c79622e144acc8032e60b392
ssy1248 <ssy1248@gmail.com>
2025-02-28 00:06:36 +0900

    chore: 스킬 액션 핸들러 수정

--- src/handlers/game/InGame/player.handler.js
+++ src/handlers/game/InGame/player.handler.js
@@ -194,13 +194,15 @@ const processSkillAttackHandler = (socket, attackerName, targetId) => {
   if (targetId <= 0) {
     console.log(`[${attackerName}] 대상이 없으므로 기본 공격 진행합니다.`);
 
-    const normalAttackResult = {
+    // 타겟팅인지 아닌지에 따라 처리리
+    const skillAttackResult = {
+      skillId: 0, // 스킬 아이디
       targetId: 0, // 대상이 없으므로 0 또는 특수값
       damageDealt: 0, // 피해량 0 (혹은 기본 데미지 적용)
     };
 
     const sPlayerActionPayload = {
-      normalAttackResult: normalAttackResult,
+      skillAttackResult,
       success: true,
       message: '공격하였습니다 (대상이 없으므로 특별한 효과 없음).',
     };
@@ -225,7 +227,7 @@ const processSkillAttackHandler = (socket, attackerName, targetId) => {
       return;
     }
     const distance = calculateDistance(posA, posB);
-    if (distance > player.normalAttack.attackRange) {
+    if (distance > player.skillAttack.attackRange) {
       console.log('타겟이 공격 범위 밖에 있습니다.');
       sendActionFailure(socket, '타겟이 공격 범위 밖에 있습니다.');
       return;
@@ -233,12 +235,13 @@ const processSkillAttackHandler = (socket, attackerName, targetId) => {
 
     // 피해량 계산 (여기서는 고정 50 데미지) -> 추후 플레이어 클래스의 데미지로 변경
     console.log(`[${attackerName}] 타겟이 사거리 내에 있습니다. 공격 진행합니다.`);
-    const normalAttackResult = {
+    const skillAttackResult = {
+      skillId: 1, // 스킬 아이디
       targetId,
       damageDealt: 50,
     };
     const payload = {
-      normalAttackResult,
+      skillAttackResult,
       success: true,
       message: '공격에 성공하였습니다.',
     };
